182b5622c017c0bcd6c66f27687049646eb0b527
murat.resulogullari <resuloglumurad@gmail.com>
2023-05-25 23:36:07 +0300

    crearted moduls and components admin ui

---
+++ Pharmacy.Client/src/app/admin/components/pharmacies/pharmacies.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PharmaciesComponent } from './pharmacies.component';
+
+
+
+@NgModule({
+  declarations: [
+    PharmaciesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PharmaciesModule { }
